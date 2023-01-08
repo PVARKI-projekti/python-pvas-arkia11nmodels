5f5eeb034fc6f74c230c3de53b3b9b006ccc6cf0
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-01-08 21:04:01 +0200

    remove gino-starlette until https://github.com/python-gino/gino-starlette/issues/37 is fixed

--- src/arkia11nmodels/models/base.py
+++ src/arkia11nmodels/models/base.py
@@ -2,27 +2,13 @@
 from typing import Any
 import uuid
 
-# This fancy lazy-loader thing will mess up IDE introspection and mypy
-# from gino.ext.starlette import Gino
-# Se we load the correct module properly
-from gino_starlette import Gino
+from gino import Gino
 from sqlalchemy.dialects.postgresql import UUID as saUUID
 import sqlalchemy as sa
 
 
-from .. import dbconfig
-
 utcnow = sa.func.current_timestamp()
-db = Gino(
-    dsn=dbconfig.DSN,
-    pool_min_size=dbconfig.POOL_MIN_SIZE,
-    pool_max_size=dbconfig.POOL_MAX_SIZE,
-    echo=dbconfig.ECHO,
-    ssl=dbconfig.SSL,
-    use_connection_for_request=dbconfig.USE_CONNECTION_FOR_REQUEST,
-    retry_limit=dbconfig.RETRY_LIMIT,
-    retry_interval=dbconfig.RETRY_INTERVAL,
-)
+db = Gino()
 DBModel: Any = db.Model  # workaround mypy being unhappy about using @property as baseclass
 
 
