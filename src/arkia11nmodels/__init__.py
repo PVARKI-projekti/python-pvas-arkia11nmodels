abb07a89297b0f736f4bf6060433a9238a4762b4
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-02-09 15:57:42 +0200

    version bump

--- src/arkia11nmodels/__init__.py
+++ src/arkia11nmodels/__init__.py
@@ -1,2 +1,2 @@
 """ Authorization related ORM models and Pydantic schemas """
-__version__ = "1.0.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "1.0.1"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
